df835e88f958e6af2e7031a175aa7332d89ddd03
shinjiyun <ranji12@naver.com>
2024-07-17 16:51:28 +0900

    기본 hello 작성

--- front-react/src/App.tsx
+++ front-react/src/App.tsx
@@ -1,28 +1,7 @@
-import React,{useState} from 'react';
+import React from 'react';
 
-
-const App = () =>{
-  const [content, newContent] = useState('');
-  const [innerContent, newInnerContent] = useState('');
-  const send=()=>{
-    const data = {content:content}
-    fetch("http://localhost:8080/send",{
-      method:"POST",
-      headers:{
-        "Content-Type":"application/json"
-      },
-      body:JSON.stringify(data)
-    })
-    .then(response=>response.json())
-    .then(data => newInnerContent(innerContent+data.content+`<br>`))
-  }
-  return(
-  <div>
-    <input type="text" value={content} onChange={(ele)=>newContent(ele.target.value)}placeholder="내용을 입력하세요"/>
-    <button  onClick={send}>전송하기</button>
-    <div id="inner">{innerContent}</div>
-  </div>
-  )
-}
+const App:React.FC = () => {
+  return <h1>Hello</h1>;
+};
 
 export default App;
