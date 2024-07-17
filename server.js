d3d73a4dcdd38a766071d9e52f6524d7b410e34c
shinjiyun <ranji12@naver.com>
2024-07-17 16:50:47 +0900

    express로 작성한 서버

--- server.js
+++ server.js
@@ -1,35 +1,14 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-  if (req.method === 'GET') {
-    if (req.url === '/') {
-      console.log("get")
-      const first = fs.readFileSync('./front-react/public/index.html', 'utf-8')
-      res.writeHead(200, { 'Content-Type': 'text/html; charset=utf8' })
-      res.end(first);
-    }
-    else if(req.url === '/index.bundle.js'){
-      const second=fs.readFileSync('./front-react/dist/index.bundle.js')
-      res.writeHead(200, { 'Content-Type': 'text/javascript; charset=utf8' })
-      res.end(second);
-    }
-  }
-  if (req.method === 'POST') {
-    if (req.url === '/send') {
-      let body = '';
-      req.on('data', (chunk) => {
-        body += chunk.toString()
-      });
-      req.on('end', () => {
-        const data = JSON.parse(body);
-        
-        console.log('받은 데이터:', data);
-        res.writeHead(200, { 'Content-Type': 'application/json' })
-        res.end(JSON.stringify(data))
+app.use(express.static(path.join(__dirname, 'dist')));
 
-      })
-    }
-  }
-})
-server.listen(8080)
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
