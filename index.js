43bdde56497c1b7a8174d3e6bee413c60713e3bd
shinjiyun <ranji12@naver.com>
2024-07-17 11:18:08 +0900

    fetch로 받아 온 데이터 처리 후 클라이언트 쪽으로 다시 보내줌

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 const http = require('http');
 const fs = require('fs');
 
-const server = http.createServer((req,res)=>{
-  if(req.method==='GET'){
-    if(req.url==='/'){
-      const first = fs.readFileSync('./index.html','utf-8')
-      res.writeHead(200,{'Content-Type':'text/html; charset=utf8'})
+const server = http.createServer((req, res) => {
+  if (req.method === 'GET') {
+    if (req.url === '/') {
+      const first = fs.readFileSync('./index.html', 'utf-8')
+      res.writeHead(200, { 'Content-Type': 'text/html; charset=utf8' })
       res.end(first);
+    }
+  }
+  if (req.method === 'POST') {
+    if (req.url === '/send') {
+      let body = '';
+      req.on('data', (chunk) => {
+        body += chunk.toString()
+      });
+      req.on('end', () => {
+        const data = JSON.parse(body);
+        
+        console.log('받은 데이터:', data);
+        res.writeHead(200, { 'Content-Type': 'application/json' })
+        res.end(JSON.stringify(data))
+
+      })
     }
   }
 })
