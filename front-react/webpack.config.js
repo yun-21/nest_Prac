c1adbddd4113bfd5b2f5263cc9f6af1201db2a11
shinjiyun <ranji12@naver.com>
2024-07-17 16:46:41 +0900

    기본설정 추가

---
+++ front-react/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+let name = "index";
+module.exports={
+  entry:`./src/${name}.tsx`,
+  output:{
+    path:path.resolve(__dirname,"dist"),
+    filename:`${name}.bundle.js`
+  },
+  module:{
+    rules:[
+      {
+        test:/\.css$/,
+        use:["style-loader","css-loader"]
+      },
+      {
+        test:/\.js|jsx|ts|tsx$/,
+        use:"babel-loader",
+        exclude:/node_modules/
+      },
+      {
+        test:/\.ts$/,
+        use:"ts-loader",
+        exclude:/node_modules/
+      }
+    ],
+  },
+  resolve:{
+    extensions: [".ts",".js",".tsx",".jsx",".css"]
+  }
+}
