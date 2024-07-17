c5b06e024bf803a23bf13a00860b7f879ad3463f
shinjiyun <ranji12@naver.com>
2024-07-17 12:45:29 +0900

    state를 사용하여 clock작성

--- front-react/src/index.jsx
+++ front-react/src/index.jsx
@@ -1,6 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import ReactDOM from 'react-dom/client'
+class Clock extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {date: new Date()};
+  }
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(<Clock />);
