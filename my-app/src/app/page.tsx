154abcf65bd8527587a353af61efc18b754ef884
신지윤 <ranji12@naver.com>
2024-07-18 18:04:43 +0900

    기본 작성

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,7 +1,16 @@
-import Image from "next/image";
+import { useRouter } from 'next/router';
 
 export default function Home() {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push('/dashboard');
+  };
+
   return (
-    <h1>Hello, Nest.js</h1>
+    <div>
+      <h1>Hello, Nest.js</h1>
+      <button type="button" onClick={handleClick}>클릭</button>
+    </div>
   );
 }
